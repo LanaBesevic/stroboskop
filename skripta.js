563b3ee109082981333ae1526c685121238d27cc
Lana Besevic <lana.besevic@gmail.com>
2016-03-20 15:38:31 +0000

    Dinamika gumba za odstranjevanje barv

--- skripta.js
+++ skripta.js
@@ -13,6 +13,16 @@ window.addEventListener('load', function() {
 		.addEventListener('click', dodajBarvo);
 		
 	//Odstrani barve
+	var odstraniBarve = function(event) {
+		var spanBarve = document.getElementById("barve");
+		
+		while (spanBarve.firstChild) {
+			spanBarve.removeChild(spanBarve.firstChild);
+		}
+	}
+	
+	document.querySelector("#odstraniBarve") 
+		.addEventListener('click', odstraniBarve);
 	
 	//Stroboskop
 	var vrednosti = [];
